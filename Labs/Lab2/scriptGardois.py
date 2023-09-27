522edd0664295b01919518fcbc43dfa5cc45f8ba
Ninja <s315223@studenti.polito.it>
2023-09-27 15:39:10 +0200

    completed 3.1 and 3.2 Lab2

---
+++ Labs/Lab2/scriptGardois.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([[1,2],[3,4]])
+b = np.array([[5, 6], [7,8]])
+np.array([1, 2])
+print(a)
+print(b)
+
+print( np.zeros(a.shape[1]) )
+
+# r = np.r_[a, b]
+# print(r)
+# c = np.c_[a, b]
+# print(c)
+
+# d = np.diff(a, n=1)
+# print(d)
